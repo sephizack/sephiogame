9ca95613dbe7c6fccd59d15c2538194bcca1b718
Imperator2Toulouse <Imperator2Toulouse@users.noreply.github.com>
2015-09-25 02:40:50 +0200

    Update GoogleAPI.js
    Add sendmail function

--- GoogleAPI.js
+++ GoogleAPI.js
@@ -64,9 +64,21 @@ function loadGmailApi() {
  //gapi.client.load('gmail', 'v1', listLabels);
 }
 
-  /**
-   * Send email
-   * @param {email address, body text, url callback}
-   */
-function sendmail(mail_address, mail, callback) {
+/**
+ * Send Message.
+ *
+ * @param  {String} userId User's email address. The special value 'me'
+ * can be used to indicate the authenticated user.
+ * @param  {String} email RFC 5322 formatted String.
+ * @param  {Function} callback Function to call when the request is complete.
+ */
+function sendMessage(userId, email, callback) {
+  var base64EncodedEmail = btoa(email);
+  var request = gapi.client.gmail.users.messages.send({
+    'userId': userId,
+    'message': {
+      'raw': base64EncodedEmail
+    }
+  });
+  request.execute(callback);
 }
