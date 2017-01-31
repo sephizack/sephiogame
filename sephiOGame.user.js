54fa621fb6f7360a2b1d661cd1bf7165b7b5d1cd
Imperator2Toulouse <Imperator2Toulouse@users.noreply.github.com>
2017-01-31 23:47:13 +0100

    Correction for bug '11'

--- sephiOGame.user.js
+++ sephiOGame.user.js
@@ -947,7 +947,7 @@ function change_actions_tab(action_tab){
                     frigData +='<input type="hidden" id="position'+(index+1)+'" value="'+(planet)+'">';
                     frigData +='<input type="hidden" id="flottes'+(index+1)+'" value="'+(flottes)+'">';
                     frigData +='<input type="hidden" id="defense'+(index+1)+'" value="'+(defense)+'">';
-                    frigData +='<input type="hidden" id="sondes'+(index+1)+'" value="'+(importvars["frigos"][num_frigo][11])+'">';
+                    frigData +='<input type="hidden" id="sondes'+(index+1)+'" value="'+((num_frigo>0&&parseInt(importvars["frigos"][num_frigo][11])>0)?parseInt(importvars["frigos"][num_frigo][11]):nb_sonde_default)+'">';
                     frigData +='</span></a>';
                     //Add the object built
                     $($(this).find('.msg_action_link.overlay')).before(frigData);
