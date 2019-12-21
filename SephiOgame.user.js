e9f4540f3b15e87d3655d360c77c9039f8122bd4
Chewbaka <3535011+Chewbaka69@users.noreply.github.com>
2019-12-21 11:22:55 +0100

    Update SephiOgame.user.js

--- SephiOgame.user.js
+++ SephiOgame.user.js
@@ -14,8 +14,8 @@
 // @copyright   2012+, You
 // @require     http://code.jquery.com/jquery-1.9.1.min.js
 // @grant       GM_xmlhttpRequest
-// @updateURL   https://github.com/Chewbaka69/sephiogame/raw/master/SephiOgame.user.js
-// @downloadURL https://github.com/Chewbaka69/sephiogame/raw/master/SephiOgame.user.js
+// @updateURL   https://raw.githubusercontent.com/Chewbaka69/sephiogame/master/SephiOgame.user.js
+// @downloadURL https://raw.githubusercontent.com/Chewbaka69/sephiogame/master/SephiOgame.user.js
 // @connect     sephiogame.com
 // ==/UserScript==
 //
